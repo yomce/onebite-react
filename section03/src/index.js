fdc4c1d63c7f7c7f66c0a3da92ad546fdf59a324
yomce <kky032911@gmail.com>
2025-06-17 13:18:09 +0900

    feat: node.js 기초 #3
    모듈 시스템 이해 및 라이브러리 사용

---
+++ section03/src/index.js
@@ -0,0 +1,17 @@
+// console.log("안녕 Node.js");
+
+// const { add, sub } = require("./math");
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+// import mul from "./math.js";
+
+// import { add, sub } from "./math.js";
+
+import mul, { add, sub } from "./math.js";
+
+import randomColor from "randomcolor";
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
---
+++ section03/src/math.js
@@ -0,0 +1,20 @@
+// math 모듈
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+// module.exports = {
+//   add: add,
+//   sub: sub,
+// };
+
+// export { add, sub };
+
+export default function multifly(a, b) {
+  return a * b;
+}
